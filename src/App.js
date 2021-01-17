fd093c383f32f25221907684ba3039559c14e7bd
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-01-17 21:46:25 +0530

    aligning components as they required

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
+import {Header, FilterNews, NewsFeed} from './components'
 import './App.css';
-import {Header , FilterNews} from './components'
 
 function App() {
-  return (
-    <div className="page">
-    <div className="header">
-    <Header/>
-    </div>
-    <div className="filterNews">
-    <FilterNews/>
-    </div>
-    </div>
-  );
+    return (
+        <div className="container-fluid app">   {/*  creating a layout of ui */}
+            <div className="row header">
+                <div className="offset-md-2">
+                    <Header/>                      {/* aligning show latest news button*/}
+                </div>
+            </div>
+            <div className="">
+              <div className="row">
+              <div className="filterNews offset-md-2">
+                    <FilterNews/>                         {/* aligning filter news and Newsfeed component side by side*/}
+                </div>
+                <div className="newsCard ml-sm-5 ">
+                    <NewsFeed/>
+                </div>
+              </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
