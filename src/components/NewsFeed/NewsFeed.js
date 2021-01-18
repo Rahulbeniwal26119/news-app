be94fc14d5f97e424be15e4e7777dcf55fa0105b
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-01-18 19:12:40 +0530

    adding some comments

--- src/components/NewsFeed/NewsFeed.js
+++ src/components/NewsFeed/NewsFeed.js
@@ -1,32 +1,108 @@
-import './NewsFeed.css';  //style sheet for NewsFeed
-export default function NewsFeed() {
+import './NewsFeed.css'; // style sheet for NewsFeed
+
+function NewsFeed() {
     return (
         <div>
-        <div className="news-card-main mt-5">  {/* news card component  */}
-            <div className="news-title">    {/* news card title*/}
-                <p>Bogus Title</p>                      
-                <p>&times;</p>                      {/* cross symbol*/}
+            <div className="news-card-main mt-4">
+                {/* news card component  */}
+                <div className="news-title">
+                    {/* news card title*/}
+                    <p>Bogus Title</p>
+                    <p>&times;</p>
+                    {/* cross symbol*/} </div>
+                <div className="text-muted">
+                    {/* author name && date */}
+                    <span className="author-name">Author Name</span>
+                    <span>
+                        |
+                    </span>
+                    <span className="publish-date">26-02-19</span>
+                </div>
+                <div className="image">
+                    {/* image for news*/}
+                    <a href="www.google.com">
+                        image link
+                    </a>
+                </div>
+                <div className="news-description">
+                    {/* news decription */}
+                    helljfdsjkl;fds fdsklj;fdsajkl;fdasjka;fds
+                </div>
+                <div className="reference">Reference</div>
+                <div className="url">
+                    {/* reference for total news*/}
+                    <a href="www.google.com">
+                        image url
+                    </a>
+                </div>
             </div>
-            <div className="text-muted">                    {/* author name && date */}
-            <span className="author-name">Author Name</span>
-             <span> | </span>
-            <span className="publish-date">26-02-19</span> 
+            <div className="news-card-main mt-4">
+                {/* news card component  */}
+                <div className="news-title">
+                    {/* news card title*/}
+                    <p>Bogus Title</p>
+                    <p>&times;</p>
+                    {/* cross symbol*/} </div>
+                <div className="text-muted">
+                    {/* author name && date */}
+                    <span className="author-name">Author Name</span>
+                    <span>
+                        |
+                    </span>
+                    <span className="publish-date">26-02-19</span>
+                </div>
+                <div className="image">
+                    {/* image for news*/}
+                    <a href="www.google.com">
+                        image link
+                    </a>
+                </div>
+                <div className="news-description">
+                    {/* news decription */}
+                    helljfdsjkl;fds fdsklj;fdsajkl;fdasjka;fds
+                </div>
+                <div className="reference">Reference</div>
+                <div className="url">
+                    {/* reference for total news*/}
+                    <a href="www.google.com">
+                        image url
+                    </a>
+                </div>
             </div>
-            <div className="image">                             {/* image for news*/}
-            <a href="#">
-                image link
-            </a>
+             <div className="news-card-main mt-4">
+                {/* news card component  */}
+                <div className="news-title">
+                    {/* news card title*/}
+                    <p>Bogus Title</p>
+                    <p>&times;</p>
+                    {/* cross symbol*/} </div>
+                <div className="text-muted">
+                    {/* author name && date */}
+                    <span className="author-name">Author Name</span>
+                    <span>
+                        |
+                    </span>
+                    <span className="publish-date">26-02-19</span>
+                </div>
+                <div className="image">
+                    {/* image for news*/}
+                    <a href="www.google.com">
+                        image link
+                    </a>
+                </div>
+                <div className="news-description">
+                    {/* news decription */}
+                    helljfdsjkl;fds fdsklj;fdsajkl;fdasjka;fds
+                </div>
+                <div className="reference">Reference</div>
+                <div className="url">
+                    {/* reference for total news*/}
+                    <a href="www.google.com">
+                        image url
+                    </a>
+                </div>
             </div>
-            <div className="news-description">                       {/* news decription */}
-            helljfdsjkl;fds fdsklj;fdsajkl;fdasjka;fds
-            </div>
-            <div className="reference">Reference</div>          
-            <div className="url">                                           {/* reference for total news*/}
-            <a href="#">
-                image url
-            </a>
-            </div>
-        </div>
         </div>
     )
 }
+export default NewsFeed;
