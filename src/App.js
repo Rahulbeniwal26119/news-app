bf6fe7cfe32910eed819c2a6922165e4e08e579f
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-01-18 17:00:51 +0530

    passing props to FilterNews to get selected fields

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
+import React from 'react';
 import {Header, FilterNews, NewsFeed} from './components'
 import './App.css';
 
-function App() {
-    return (
-        <div className="container-fluid app">   {/*  creating a layout of ui */}
-            <div className="row header">
-                <div className="offset-md-2">
-                    <Header/>                      {/* aligning show latest news button*/}
+class App extends React.Component {
+
+    state = {
+        selectedOption: {
+            language: "",
+            country: "",
+            startDate: "",
+            endDate: ""
+        },
+        dataFetched: "",
+        urlToken: "iLE8hxgNBYr68Uwtg74wKEMdy7qv0XZ643dwp3uubbrZOxwa"
+    }
+
+
+    getOptions = (language, country, startDate, endDate) => {
+        this.setState({selectedOption : {language: language, country: country, startDate: startDate, endDate: endDate}})
+    }
+
+    componentDidMount() {
+    }
+
+    render() {
+        console.log(this.state.selectedOption)
+        return (
+            <div className="container-fluid app">
+                {/*  creating a layout of ui */}
+                <div className="row header">
+                    <div className="offset-md-2">
+                        <Header/> {/* aligning show latest news button*/} </div>
+                </div>
+                <div className="">
+                    <div className="row">
+                        <div className="filterNews offset-md-2">
+                            <FilterNews getOptions={
+                                this.getOptions
+                            }/> {/* aligning filter news and Newsfeed component side by side */} </div>
+                        <div className="newsCard ml-sm-5 ">
+                            <NewsFeed/>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="">
-              <div className="row">
-              <div className="filterNews offset-md-2">
-                    <FilterNews/>                         {/* aligning filter news and Newsfeed component side by side*/}
-                </div>
-                <div className="newsCard ml-sm-5 ">
-                    <NewsFeed/>
-                </div>
-              </div>
-            </div>
-        </div>
-    );
+        );
+    }
+
 }
 
 export default App;
