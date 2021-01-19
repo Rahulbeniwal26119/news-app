9ba2bd74fb049345433d0a64058dba5e8a887432
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-01-19 18:16:33 +0530

    FilterNews state is now can be managed by App.js

--- src/App.js
+++ src/App.js
@@ -6,29 +6,32 @@ class App extends React.Component {
     constructor() {
         super();
         this.state = {
-                language: "",
-                country: "",
-                startDate: "",
-                endDate: "",
-                fetchNews : []
+            language: "",
+            country: "",
+            startDate: "",
+            endDate: "",
+            fetchNews: []
         }
         this.loading = true;
-        this.urlToken =  "WY4oDcSkQmhB583VJkchnKszhoWUL3bzi9mHRcBkPssA0yGQ";
+        this.getLatestPressed = false;
+        this.urlToken = "c8yIkB0OIX63tq8LKVpnmul0QhAKsxkwJLzKOYbe5GukV6W9";
         this.url = ``
     }
 
-    getLatest = ()=>{
-        console.log("in get latest");
-        this.url = `https://api.currentsapi.services/v1/latest-news?apiKey=${this.urlToken}`;
+    getLatest = () => {
+        this.getLatestPressed = true;
+        this.url = `https://api.currentsapi.services/v1/latest-news?apiKey=${
+            this.urlToken
+        }`;
         axios.get(this.url).then(response => {
             this.setState(prevState => {
-                
+
                 return {
                     language: "",
                     country: "",
-                    startDate : "",
-                    endDate : "",
-                    fetchedNews : response.data.news
+                    startDate: "",
+                    endDate: "",
+                    fetchedNews: response.data.news
                 }
             })
         })
@@ -36,60 +39,74 @@ class App extends React.Component {
 
     getOptions = (language, country, startDate, endDate) => {
         this.setState({
-                language: language,
-                country: country,
-                startDate: startDate,
-                endDate: endDate,
-                fetchNews : []
+            language: language,
+            country: country,
+            startDate: startDate,
+            endDate: endDate,
+            fetchNews: []
         })
     }
 
-    
-    
-    getNews = (country , language)=>{
-        // https://api.currentsapi.services/v1/latest-news?apiKey=${this.urlToken}`
-        this.url = `https://api.currentsapi.services/v1/search?country=${country}&language=${language}&apiKey=${this.urlToken}`;
-        console.log(this.url)
+
+    getNews = (country, language) => { // https://api.currentsapi.services/v1/latest-news?apiKey=${this.urlToken}`
+        this.getLatestPressed = false;
+        this.url = `https://api.currentsapi.services/v1/search?country=${country}&language=${language}&apiKey=${
+            this.urlToken
+        }`;
         axios.get(this.url).then(response => {
             this.setState(prevState => {
                 return {
                     language: prevState.language,
                     country: prevState.country,
-                    startDate : prevState.startDate,
-                    endDate : prevState.endDate,
-                    fetchedNews : response.data.news
+                    startDate: prevState.startDate,
+                    endDate: prevState.endDate,
+                    fetchedNews: response.data.news
                 }
             })
         })
     }
 
-    componentDidMount() {
+    componentDidMount() { 
         this.getLatest();
         this.loading = false;
     }
-    
+
     render() {
-        // this.getLatest();
-        // console.log(this.state)
-        // console.log(this.url)
         return (
             <div className="container-fluid app">
                 {/*  creating a layout of ui */}
                 <div className="row header">
                     <div className="offset-md-2">
-                        <Header latestNews={this.getLatest}/> {/* aligning show latest news button*/} </div>
+                        <Header latestNews={
+                                this.getLatest
+                            }
+                            buttonStatus={
+                                this.getLatestPressed
+                            }/> {/* aligning show latest news button*/} </div>
                 </div>
                 <div className="">
                     <div className="row">
                         <div className="filterNews offset-md-2">
                             <FilterNews getOptions={
-                                this.getOptions
-                            } reRender = {this.getNews}/> {/* aligning filter news and Newsfeed component side by side */} </div>
+                                    this.getOptions
+                                }
+                                reRender={
+                                    this.getNews
+                                }
+                                buttonStatus={
+                                    this.getLatestPressed
+                                }/> {/* aligning filter news and Newsfeed component side by side */} </div>
                         <div className="newsCard ml-sm-5 ">
                             {
-                                this.loading ? <div> <img src={require("./components/loading.svg").default} alt=""/>  </div> : <NewsFeed newsData={this.state.fetchedNews ? this.state.fetchedNews : ""}/>
-                            }
-                        </div>
+                            this.loading ? <div height="15" width="20" className="mt-4">
+                                <img src={
+                                        require("./components/loading.svg").default
+                                    }
+                                    alt=""/>
+                            </div> : <NewsFeed newsData={
+                                this.state.fetchedNews ? this.state.fetchedNews : ""
+                            }/>
+                        } </div>
                     </div>
                 </div>
             </div>
